8ca9edee69693c01b3769e76a308ef4e098dc913
Ajit <sutarajit7276@gmail.com>
2021-04-06 09:18:52 +0530

    Customer view data

--- main/src/app/Main/customer/customer-routing.module.ts
+++ main/src/app/Main/customer/customer-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CustomerListComponent } from './customer-list/customer-list.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { CustomerViewComponent } from './customer-view/customer-view.component';
 
 const routes: Routes = [
 
   {path:'', component:DashboardComponent},
   {path:'dashboard', component:DashboardComponent},
-  {path:'customer-list',component:CustomerListComponent}
+  {path:'customer-list',component:CustomerListComponent},
+  {path:'view',component:CustomerViewComponent}
 
   
 
--- main/src/app/Main/customer/customer.module.ts
+++ main/src/app/Main/customer/customer.module.ts
@@ -6,13 +6,19 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { CustomerListComponent } from './customer-list/customer-list.component';
 import { CustomerAddComponent } from './customer-add/customer-add.component';
 import { CustomerEditComponent } from './customer-edit/customer-edit.component';
+import { CustomerViewComponent } from './customer-view/customer-view.component';
+import { AgmCoreModule } from '@agm/core';
+import { RouterModule } from '@angular/router';
+// import 
 
 
 @NgModule({
-  declarations: [DashboardComponent, CustomerListComponent, CustomerAddComponent, CustomerEditComponent],
+  declarations: [DashboardComponent, CustomerListComponent, CustomerAddComponent, CustomerEditComponent, CustomerViewComponent],
   imports: [
     CommonModule,
+    AgmCoreModule,
     CustomerRoutingModule
+    // RouterModule.forChild(CustomerRoutingModule)
   ]
 })
 export class CustomerModule { }
