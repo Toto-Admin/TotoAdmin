f2d06a8d5cd0ff12c06a25dba0bf78651ba8dc4f
Ajit <sutarajit7276@gmail.com>
2021-04-13 07:42:19 +0530

    UI changes Ajit

--- main/src/app/Main/customer/customer-list/customer-list.component.ts
+++ main/src/app/Main/customer/customer-list/customer-list.component.ts
@@ -1,14 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { User } from '../user';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { DataTableDirective } from 'angular-datatables';
 
 @Component({
   selector: 'app-customer-list',
   templateUrl: './customer-list.component.html',
   styleUrls: ['./customer-list.component.css']
 })
-export class CustomerListComponent implements OnInit {
+export class CustomerListComponent implements OnInit,AfterViewInit {
+  @ViewChild(DataTableDirective, { static: false })
+  datatableElement!: DataTableDirective;
   config: any;
   // editUser: FormGroup | null = null;
   userDetail: User | null = null;
@@ -22,9 +25,13 @@ export class CustomerListComponent implements OnInit {
   constructor(private fb: FormBuilder,private modalService: NgbModal) { }
   formsErrors = [];
   ngOnInit(): void {
+    
     this.dtOptions = {
       pagingType: 'full_numbers',
-      pageLength:5,
+      pageLength:8,
+      ordering : false,
+      lengthMenu :[[8,25,50,100,-1],[8,25,50,100,'All']],
+     
     };
     this.editUser = this.fb.group({
       id: [''],
@@ -36,6 +43,23 @@ export class CustomerListComponent implements OnInit {
       Salary: ['', Validators.required],
       Projects: ['', Validators.required],
   });
+  }
+  ngAfterViewInit(): void {
+    this.datatableElement.dtInstance.then((dtInstance: DataTables.Api) => {
+      dtInstance.columns().every(function () {
+        const that = this;
+        $('input', this.header()).on('keyup change', function () {
+          debugger
+          var data :any;
+          data = $(this).val();
+          if (that.search() !== data) {
+            that
+              .search(data)
+              .draw();
+          }
+        });
+      });
+    });
   }
 
   openModal(targetModal: NgbModal, user: User | null) {
--- main/src/app/Main/provider/provider-list/provider-list.component.ts
+++ main/src/app/Main/provider/provider-list/provider-list.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit} from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
-import { Provider } from '../provider';
+// import { User } from '../user';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { DataTableDirective } from 'angular-datatables';
 
 @Component({
   selector: 'app-provider-list',
@@ -10,39 +11,64 @@ import { Provider } from '../provider';
 })
 export class ProviderListComponent implements OnInit {
 
+  @ViewChild(DataTableDirective, { static: false })
+  datatableElement!: DataTableDirective;
   config: any;
   // editUser: FormGroup | null = null;
-  providerDetail: Provider | null = null;
+  // userDetail: User | null = null;
 
-  filterArray: Provider[] | null = null;
+  // filterArray: User[] | null = null;
 
- 
+  joiningDate: string | null = null;
   editUser: FormGroup | null = null;
+  dtOptions: DataTables.Settings = {};
 
   constructor(private fb: FormBuilder,private modalService: NgbModal) { }
   formsErrors = [];
-  
   ngOnInit(): void {
-
-   
-
+    
+    this.dtOptions = {
+      pagingType: 'full_numbers',
+      pageLength:8,
+      ordering : false,
+      lengthMenu :[[8,25,50,100,-1],[8,25,50,100,'All']],
+     
+    };
     this.editUser = this.fb.group({
       id: [''],
       Name: ['', Validators.required],
+      Position: ['', Validators.required],
       Email: ['', Validators.required],
       Mobile: ['', Validators.required],
-      
+      DateOfJoining: ['', Validators.required],
+      Salary: ['', Validators.required],
+      Projects: ['', Validators.required],
   });
-  
-  
   }
-
-  openModal(targetModal: NgbModal, provider: Provider | null) {
-    this.modalService.open(targetModal, {
-        centered: true,
-        backdrop: 'static'
+  ngAfterViewInit(): void {
+    this.datatableElement.dtInstance.then((dtInstance: DataTables.Api) => {
+      dtInstance.columns().every(function () {
+        const that = this;
+        $('input', this.header()).on('keyup change', function () {
+          debugger
+          var data :any;
+          data = $(this).val();
+          if (that.search() !== data) {
+            that
+              .search(data)
+              .draw();
+          }
+        });
+      });
     });
   }
+
+  // openModal(targetModal: NgbModal, user: User | null) {
+  //   this.modalService.open(targetModal, {
+  //       centered: true,
+  //       backdrop: 'static'
+  //   });
+  // }
   closeBtnClick() {
     this.modalService.dismissAll();
     this.ngOnInit();
