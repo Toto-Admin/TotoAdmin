80e16658df099b59133cff8a519a17c89731a9c5
Ajit <sutarajit7276@gmail.com>
2021-04-19 12:04:56 +0530

    Changes 19/4

--- main/src/app/Main/customer/customer-list/customer-list.component.ts
+++ main/src/app/Main/customer/customer-list/customer-list.component.ts
@@ -31,7 +31,9 @@ export class CustomerListComponent implements OnInit,AfterViewInit {
       pageLength:10,
       ordering : true,
       lengthMenu :[[10,25,50,100,-1],[10,25,50,100,'All']],
-     
+      columnDefs: [
+        { orderable: false, targets: -1 }
+     ]
     };
     this.editUser = this.fb.group({
       id: [''],
