8e3fd484328e5aba127749919417aa0ce02fb78e
TotoAdmin <developers@totoapp.co.uk>
2021-04-20 19:17:37 +0530

    knowledge-add-adminpanel

---
+++ main/src/app/Main/mobile-apps/knowledg-details/knowledg-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-knowledg-details',
+  templateUrl: './knowledg-details.component.html',
+  styleUrls: ['./knowledg-details.component.css']
+})
+export class KnowledgDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
